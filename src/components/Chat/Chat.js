0e94b374439dea3d8f9bb1a6afab88610e4dc99f
MiihSoraya <michelesoraya1@gmail.com>
2022-07-10 15:42:58 -0300

    mudando pra backend de pedro

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ export const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://localhost:3000';
+  const ENDPOINT = 'https://paloo-chat-backend.herokuapp.com';
 
 
   useEffect(() => {
