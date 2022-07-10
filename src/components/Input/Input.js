60f3a23cfab1136314b294000b8979c165b052f4
MiihSoraya <michelesoraya1@gmail.com>
2022-07-09 22:33:44 -0300

    chat

---
+++ src/components/Input/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'font-awesome/css/font-awesome.min.css';
+
+import './Input.css';
+
+export const Input = ({ message, setMessage, sendMessage }) => (
+    <form className="form">
+    <input 
+    className="input" 
+    type="text" 
+    placeholder="Escreva a mensagem..." 
+    value={message} 
+    onChange={(e) => setMessage(e.target.value)} 
+    onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null }
+    />
+    <button className="sendButton" onClick={(e) => sendMessage(e)}><div><i className="fa fa-spin fa-chevron-circle-right" /></div></button>
+    </form>
+);
---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+import img1 from '../../assets/logo.png';
+
+import './TextContainer.css';
+
+export const TextContainer = ({ users }) => (
+  <div className='textContainer'>
+    <div className='logo'>
+        <img src={img1}/>
+    </div>
+    {users ? (
+      <div>
+        <h1 className='online'>USUÁRIOS ONLINE</h1>
+        <div className='activeContainer'>
+          <h2>
+            {users.map(({ name }) => (
+              <div key={name} className='activeItem'>
+                {name}
+                <img alt='Online Icon' src={onlineIcon} />
+              </div>
+            ))}
+          </h2>
+        </div>
+      </div>
+    ) : null}
+  </div>
+);
