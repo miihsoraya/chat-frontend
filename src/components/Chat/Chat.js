14a896e1e096d17bfbdc0dda04a41001b037f290
MiihSoraya <michelesoraya1@gmail.com>
2022-07-11 16:16:23 -0300

    atualizando interface

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ export const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'http://localhost:5000';
+  const ENDPOINT = 'https://paloo-chat-backend.herokuapp.com';
 
 
   useEffect(() => {
